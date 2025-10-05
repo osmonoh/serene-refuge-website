1ad2f4db7846bdb979f135fb5188fc36e5fa97e0
dusan chmelko <dusan.chmelko@yahoo.com>
2025-10-05 17:20:55 +0200

    add supabase, date-fns, spinner, cabin pages

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,12 @@
+import { Suspense } from "react";
+import CabinList from "@/app/_components/CabinList";
+import Spinner from "@/app/_components/Spinner";
+
 export const metadata = {
     title: "Cabins"
 };
 
 export default function Page() {
-    const cabins = [];
-
     return (
         <div>
             <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -19,11 +21,9 @@ export default function Page() {
                 perfect spot for a peaceful, calm vacation. Welcome to paradise.
             </p>
 
-            {cabins.length > 0 && (
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-                    CABIN CARDS
-                </div>
-            )}
+            <Suspense fallback={<Spinner />}>
+                <CabinList />
+            </Suspense>
         </div>
     );
 }
